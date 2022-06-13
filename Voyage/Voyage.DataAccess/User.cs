779f849ff98969790af001d5edf1f806367cbab3
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-13 19:42:12 +0300

    AddEntitiesStage1
    Entities User and Role are added

---
+++ Voyage/Voyage.DataAccess/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage.DataAccess
+{
+    internal class User
+    {
+        public int UserId { get; set; } //первичный ключ 
+        public int RoleId { get; set; } //внешний ключ
+        public Role Role { get; set; } //навигационное свойство
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+    }
+}

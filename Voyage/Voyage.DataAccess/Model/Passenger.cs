a390fe09c7452c2bb457bfd83be145b54be94cd1
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-13 23:52:31 +0300

    AddEntitiesStage2
    Entities Passenger and Ticket has been added

---
+++ Voyage/Voyage.DataAccess/Model/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage.DataAccess
+{
+    internal class Passenger
+    {
+        [Key]
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int TripsCount { get; set; }
+        public int Points { get; set; }
+    }
+}
---
+++ Voyage/Voyage.DataAccess/Model/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage.DataAccess
+{
+    internal class Ticket
+    {
+        public int PassengerId { get; set; }//внешний ключ
+        public Passenger Passenger { get; set; }//навигационное свойство
+
+        /*
+        public int TripId { get; set; }//внешний ключ
+        public Trip Trip { get; set; }//навигационное свойство
+        */
+
+        //эти ключи так же первичные.Донастрою при настройке контекста
+
+        public DateTime PuchaseDate { get; set; }
+        public decimal Cost { get; set; }
+
+    }
+}

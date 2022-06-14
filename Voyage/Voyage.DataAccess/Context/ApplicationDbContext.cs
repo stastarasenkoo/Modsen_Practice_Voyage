813f37091353fa5c4b13f2679186802b06aa2cc9
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-14 15:23:43 +0300

    Create ApplicationDBContext and add models

---
+++ Voyage/Voyage.DataAccess/Context/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Voyage.DataAccess.Model;
+
+namespace Voyage.DataAccess.Context
+{
+    internal class ApplicationDbContext:DbContext
+    {
+        DbSet<Driver> Drivers { get; set; }
+        DbSet<Passenger> Passengers { get; set; }
+        DbSet<Role> Roles { get; set; }
+        DbSet<Route> Routes { get; set; }
+        DbSet<Ticket> Tickets { get; set; }
+        DbSet<TransportType> TransportTypes { get; set; }
+        DbSet<Trip> Trips { get; set; }
+        DbSet<User> Users { get; set; }
+
+    }
+}

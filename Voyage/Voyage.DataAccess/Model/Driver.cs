7390f5290b4efdbea9feb1741bc510bf85ad62e6
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-14 14:32:17 +0300

    AddEntitiesStage4
    Entities Trip and Driver has been added

---
+++ Voyage/Voyage.DataAccess/Model/Driver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage.DataAccess.Model
+{
+    internal class Driver
+    {
+        public int UserId { get; set; } //primary and foreign key
+        public int DriverExperience { get; set; }
+        public string DriverCategory { get; set; }
+        public int TripCounts { get; set; }
+    }
+}
--- Voyage/Voyage.DataAccess/Model/Ticket.cs
+++ Voyage/Voyage.DataAccess/Model/Ticket.cs
@@ -10,14 +10,9 @@ namespace Voyage.DataAccess.Model
     {
         public int PassengerId { get; set; }//foreign key
         public Passenger Passenger { get; set; }//navigational property
-
-        /*
         public int TripId { get; set; }//foreign key
         public Trip Trip { get; set; }//navigational property
-        */
-
         //This keys also primary.Fix later when create context
-
         public DateTime PuchaseDate { get; set; }
         public decimal Cost { get; set; }
 

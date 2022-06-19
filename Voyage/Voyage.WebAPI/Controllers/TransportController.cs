1aadf3d4c2f0cc4c4fafcf53bd3b62b183bd6d8f
Tarasenko Stanislav <64153866+stasyan1@users.noreply.github.com>
2022-06-19 23:18:33 +0300

    refactor program.cs

--- Voyage/Voyage.WebAPI/Controllers/TransportController.cs
+++ Voyage/Voyage.WebAPI/Controllers/TransportController.cs
@@ -11,7 +11,7 @@ namespace Voyage.WebAPI.Controllers
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize(AuthenticationSchemes = "Bearer")]
+    [Authorize]
     public class TransportController : ControllerBase
     {
         private readonly ITransportService service;
--- Voyage/Voyage.WebAPI/Options/ConfigureSwaggerOptions.cs
+++ Voyage/Voyage.WebAPI/Options/ConfigureSwaggerOptions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
 
@@ -10,6 +11,30 @@ namespace Voyage.WebAPI.Options
         {
             var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
             options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Name = "Authorization",
+                Description = "Bearer Authentication with JWT Token",
+                Type = SecuritySchemeType.Http
+            });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+              {
+                new OpenApiSecurityScheme
+                {
+                   Reference = new OpenApiReference
+                   {
+                       Id = "Bearer",
+                       Type = ReferenceType.SecurityScheme
+                   }
+                },
+                new List<string>()
+              }
+            });
         }
     }
 }

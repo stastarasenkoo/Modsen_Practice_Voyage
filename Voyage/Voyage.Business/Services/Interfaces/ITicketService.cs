9d4a1f5951a4c8bd8b1146fb7e91ee9bd9b3b0fa
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-20 18:57:21 +0300

    Add ITicketService.cs & TicketService.cs

---
+++ Voyage/Voyage.Business/Services/Interfaces/ITicketService.cs
@@ -0,0 +1,21 @@
+﻿using Voyage.Common.Dtos;
+using Voyage.Common.RequestModels;
+using Voyage.Common.ResponseModels;
+
+namespace Voyage.Business.Services.Interfaces
+{
+    public interface ITicketService
+    {
+        Task<TicketDetailsResponse> GetTicketDetailsAsync(GetTicketDetailsRequest request);
+
+        Task<IEnumerable<TicketShortInfoResponse>> GetByPassengerIdAsync(int passengerId);
+
+        Task<IEnumerable<TicketShortInfoResponse>> GetByTripIdAsync(int tripId);
+
+        Task<IEnumerable<TicketDto>> GetAsync();
+
+        Task<TicketDetailsResponse> CreateAsync(TicketDto ticketDto);
+
+        Task<bool> Delete(DeleteTicketRequest request);
+    }
+}
---
+++ Voyage/Voyage.Business/Services/TicketService.cs
@@ -0,0 +1,49 @@
+﻿using Voyage.Business.Services.Interfaces;
+using Voyage.Common.Dtos;
+using Voyage.Common.RequestModels;
+using Voyage.Common.ResponseModels;
+using Voyage.DataAccess.Repositories.Interfaces;
+
+namespace Voyage.Business.Services
+{
+    internal class TicketService : ITicketService
+    {
+        private readonly ITicketRepository repository;
+        public TicketService(ITicketRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task<TicketDetailsResponse> CreateAsync(TicketDto ticketDto)
+        {
+            var ticket = await repository.CreateAsync(ticketDto);
+
+            return ticket;
+        }
+
+        public Task<bool> Delete(DeleteTicketRequest request)
+        {
+            return repository.Delete(request);
+        }
+
+        public async Task<IEnumerable<TicketDto>> GetAsync()
+        {
+            return await repository.GetAsync();
+        }
+
+        public async Task<IEnumerable<TicketShortInfoResponse>> GetByPassengerIdAsync(int passengerId)
+        {
+            return await repository.GetByPassengerIdAsync(passengerId);
+        }
+
+        public async Task<IEnumerable<TicketShortInfoResponse>> GetByTripIdAsync(int tripId)
+        {
+            return await repository.GetByTripIdAsync(tripId);
+        }
+
+        public async Task<TicketDetailsResponse> GetTicketDetailsAsync(GetTicketDetailsRequest request)
+        {
+            return await repository.GetTicketDetailsAsync(request);
+        }
+    }
+}

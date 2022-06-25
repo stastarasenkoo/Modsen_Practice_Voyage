45f7683f6cd7c11647c62c6fc2c738335ee15a93
TimOfey25Lebiaphan <73393865+TimOfey25Lebiaphan@users.noreply.github.com>
2022-06-26 02:59:44 +0300

    Adding of functional files
    Add CreatePassengerRequest, UpdatePassengerRequest, PassengerDetailsResponse, PassengerShortInfoResponse, IPassengerRepository, PassengerRepository

---
+++ Voyage/Voyage.Common/ResponseModels/PassengerShortInfoResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Voyage.Common.ResponseModels
+{
+    public class PassengerShortInfoResponse
+    {
+        public int UserId { get; set; }
+
+        public int Points { get; set; }
+    }
+}
---
+++ Voyage/Voyage.DataAccess/Repositories/Interfaces/IPassengerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Voyage.Common.RequestModels;
+using Voyage.Common.ResponseModels;
+
+namespace Voyage.DataAccess.Repositories.Interfaces
+{
+    internal interface IPassengerRepository
+    {
+        Task<PassengerDetailsResponse?> GetByIdAsync(int id, CancellationToken cancellationToken);
+
+        Task<IEnumerable<PassengerShortInfoResponse>> GetAsync(int page, CancellationToken cancellationToken);
+
+        Task<PassengerDetailsResponse> CreateAsync(CreatePassengerRequest request, CancellationToken cancellationToken);
+
+        Task<PassengerDetailsResponse?> UpdateAsync(UpdatePassengerRequest request, CancellationToken cancellationToken);
+
+        Task<bool> DeleteAsync(int id, CancellationToken cancellationToken);
+    }
+}

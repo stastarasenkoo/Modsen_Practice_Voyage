06154fa954658c52baec0e402a7b83ab4e80193a
Elvis <abene197@gmail.com>
2022-06-25 10:37:04 +0300

    added elasticsearch logging

--- Voyage/Voyage.WebAPI/Program.cs
+++ Voyage/Voyage.WebAPI/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.Elasticsearch;
 using Voyage.Common.Settings;
 using Voyage.Dependencies;
 using Voyage.WebAPI.Options;
@@ -8,7 +10,12 @@ Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((context,services, configuration) => configuration
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName}-{DateTime.Now:yyyy-MM}"
+    })
     .ReadFrom.Configuration(context.Configuration)
     .ReadFrom.Services(services));
 
